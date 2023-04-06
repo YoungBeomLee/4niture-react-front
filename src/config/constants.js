9ffa98a09646eb0d585a5cf9236132256c26ee28
YoungBeomLee <edlee127127@gmail.com>
2023-04-06 12:22:18 +0900

    클라우드 서버주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 export const API_URL=process.env.NODE_ENV==="production"? 
-"https://port-0-furnitureserver-nx562olfdt8jh2.sel3.cloudtype.app":"http://localhost:8080";
+"https://port-0-furnitureserver-du3j2blg4i5inn.sel3.cloudtype.app":"http://localhost:8080";
